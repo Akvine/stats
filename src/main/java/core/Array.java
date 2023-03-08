4496f4c985de7472657a5537dd7ac7bd15af3f2c
akvine <redgooseman55@gmail.com>
2023-03-09 02:52:08 +0300

    add additional api in array

--- src/main/java/core/Array.java
+++ src/main/java/core/Array.java
@@ -42,6 +42,164 @@ public class Array {
     }
 
     public double[] values() {
+        return copy();
+    }
+
+    public Array plus(double value) {
+        double[] values = copy();
+        for (int i = 0; i < values.length; ++i) {
+            values[i] += value;
+        }
+        return new Array(values);
+    }
+
+    public Array minus(double value) {
+        double[] values = copy();
+        for (int i = 0; i < values.length; ++i) {
+            values[i] -= value;
+        }
+        return new Array(values);
+    }
+
+    public Array multiply(double value) {
+        double[] values = copy();
+        for (int i = 0; i < values.length; ++i) {
+            values[i] *= value;
+        }
+        return new Array(values);
+    }
+
+    public Array divide(double value) {
+        double[] values = copy();
+        for (int i = 0; i < values.length; ++i) {
+            values[i] /= value;
+        }
+        return new Array(values);
+    }
+
+    public Array pow(double value) {
+        double[] values = copy();
+        for (int i = 0; i < values.length; ++i) {
+            values[i] = Math.pow(values[i], value);
+        }
+        return new Array(values);
+    }
+
+    public Array sqrt() {
+        return pow(0.5);
+    }
+
+    public Array reverse() {
+        double[] values = new double[array.length];
+        int k = 0;
+        for (int i = array.length - 1; i >= 0; i--) {
+            values[k] = array[i];
+            k++;
+        }
+        return new Array(values);
+    }
+
+    public Array abs() {
+        double[] values = copy();
+        for (int i = 0; i < values.length; ++i) {
+            values[i] = Math.abs(values[i]);
+        }
+        return new Array(values);
+    }
+
+    public Array negative() {
+        return multiply(-1);
+    }
+
+    public Array round(int accuracy) {
+        if (accuracy < 0) {
+            throw new RuntimeException("Accuracy is less than 0");
+        }
+
+        double[] values = copy();
+        double coeff = Math.pow(10, accuracy);
+
+        for (int i = 0; i < values.length; ++i) {
+            values[i] = Math.round(values[i] * coeff) / coeff;
+        }
+
+        return new Array(values);
+    }
+
+    public Array sin() {
+        double[] values = copy();
+        for (int i = 0; i < values.length; ++i) {
+            values[i] = Math.sin(values[i]);
+        }
+        return new Array(values);
+    }
+
+    public Array cos() {
+        double[] values = copy();
+        for (int i = 0; i < values.length; ++i) {
+            values[i] = Math.cos(values[i]);
+        }
+        return new Array(values);
+    }
+
+    public Array tan() {
+        double[] values = copy();
+        for (int i = 0; i < values.length; ++i) {
+            values[i] = Math.tan(values[i]);
+        }
+        return new Array(values);
+    }
+
+    public Array sinh() {
+        double[] values = copy();
+        for (int i = 0; i < values.length; ++i) {
+            values[i] = Math.sinh(values[i]);
+        }
+        return new Array(values);
+    }
+
+    public Array cosh() {
+        double[] values = copy();
+        for (int i = 0; i < values.length; ++i) {
+            values[i] = Math.cosh(values[i]);
+        }
+        return new Array(values);
+    }
+
+    public Array tanh() {
+        double[] values = copy();
+        for (int i = 0; i < values.length; ++i) {
+            values[i] = Math.tanh(values[i]);
+        }
+        return new Array(values);
+    }
+
+    public Array acos() {
+        double[] values = copy();
+        for (int i = 0; i < values.length; ++i) {
+            values[i] = Math.acos(values[i]);
+        }
+        return new Array(values);
+    }
+
+    public Array asin() {
+        double[] values = copy();
+        for (int i = 0; i < values.length; ++i) {
+            values[i] = Math.asin(values[i]);
+        }
+        return new Array(values);
+    }
+
+    public Array atan() {
+        double[] values = copy();
+        for (int i = 0; i < values.length; ++i) {
+            values[i] = Math.atan(values[i]);
+        }
+        return new Array(values);
+    }
+
+
+    private double[] copy() {
         return Arrays.copyOf(array, array.length);
     }
 
--- src/test/java/core/ArrayTest.java
+++ src/test/java/core/ArrayTest.java
@@ -54,6 +54,232 @@ class ArrayTest {
         assertThat(array.length()).isEqualTo(size);
     }
 
+    @Test
+    @DisplayName("Plus - case 1")
+    void plus_case1() {
+        int size = 5;
+        Array array = new Array(size);
+        double[] expected = fillExpected(size, 5);
+        double[] actual = array.plus(5).values();
+
+        assertThat(array).isNotNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Plus - case 2")
+    void plus_case2() {
+        int size = 5;
+        Array array = new Array(size, 7);
+        double[] expected = fillExpected(size, 19);
+        double[] actual = array.plus(12).values();
+
+        assertThat(array).isNotNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Minus - case 1")
+    void minus_case1() {
+        int size = 5;
+        Array array = new Array(size);
+        double[] expected = fillExpected(size, -5);
+        double[] actual = array.minus(5).values();
+
+        assertThat(array).isNotNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Minus - case 2")
+    void minus_case2() {
+        int size = 5;
+        Array array = new Array(size, 25.125);
+        double[] expected = fillExpected(size, 10);
+        double[] actual = array.minus(15.125).values();
+
+        assertThat(array).isNotNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Multiply - case 1")
+    void multiply_case1() {
+        int size = 5;
+        Array array = new Array(size);
+        double[] expected = fillExpected(size, 0);
+        double[] actual = array.multiply(5).values();
+
+        assertThat(array).isNotNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Multiply - case 2")
+    void multiply_case2() {
+        int size = 5;
+        Array array = new Array(size, 5);
+        double[] expected = fillExpected(size, -25);
+        double[] actual = array.multiply(-5).values();
+
+        assertThat(array).isNotNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Divide - case 1")
+    void divide_case1() {
+        int size = 5;
+        Array array = new Array(size, 5);
+        double[] expected = fillExpected(size, -1);
+        double[] actual = array.divide(-5).values();
+
+        assertThat(array).isNotNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Divide - case 2")
+    void divide_case2() {
+        int size = 10;
+        Array array = new Array(size, 8);
+        double[] expected = fillExpected(size, -3.2);
+        double[] actual = array.divide(-2.5).values();
+
+        assertThat(array).isNotNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Pow - case 1")
+    void pow_case1() {
+        double[] values = {1, 2, 3, 4, 5};
+        double[] expected = {1, 4, 9, 16, 25};
+
+        Array array = new Array(values);
+        double[] actual = array.pow(2).values();
+
+        assertThat(array).isNotNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Pow - case 2")
+    void pow_case2() {
+        double[] values = {1, 2, 3, 4, 5};
+        double[] expected = {1, 8, 27, 64, 125};
+
+        Array array = new Array(values);
+        double[] actual = array.pow(3).values();
+
+        assertThat(array).isNotNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Sqrt - case 1")
+    void sqrt_case1() {
+        double[] values = {1, 4, 9, 16, 25};
+        double[] expected = {1, 2, 3, 4, 5};
+
+        Array array = new Array(values);
+        double[] actual = array.sqrt().values();
+
+        assertThat(array).isNotNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Sqrt - case 2")
+    void sqrt_case2() {
+        double[] values = {1, 1, 9, 36, 0.25};
+        double[] expected = {1, 1, 3, 6, 0.5};
+
+        Array array = new Array(values);
+        double[] actual = array.sqrt().values();
+
+        assertThat(array).isNotNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Reverse - case 1")
+    void reverse_case1() {
+        double[] values = {1, -2, 3, -4, 5};
+        double[] expected = {5, -4, 3, -2, 1};
+
+        Array array = new Array(values);
+        double[] actual = array.reverse().values();
+
+        assertThat(array).isNotNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Reverse - case 2")
+    void reverse_case2() {
+        double[] values = {-3, 15, 2, 0, 1, -2, 3, -4, 5};
+        double[] expected = {5, -4, 3, -2, 1, 0, 2, 15, -3};
+
+        Array array = new Array(values);
+        double[] actual = array.reverse().values();
+
+        assertThat(array).isNotNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Abs - case 1")
+    void abs_case1() {
+        double[] values = {-5, 3, 1, -4, 2, 1};
+        double[] expected = {5, 3, 1, 4, 2, 1};
+
+        Array array = new Array(values);
+        double[] actual = array.abs().values();
+
+        assertThat(array).isNotNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Abs - case 2")
+    void abs_case2() {
+        double[] values = {5, 3, 1, 4, 2, 1};
+        double[] expected = {5, 3, 1, 4, 2, 1};
+
+        Array array = new Array(values);
+        double[] actual = array.abs().values();
+
+        assertThat(array).isNotNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Negative - case 1")
+    void negative_case1() {
+        double[] values = {-5, 3, -1, 4, -2, 1};
+        double[] expected = {5, -3, 1, -4, 2, -1};
+
+        Array array = new Array(values);
+        double[] actual = array.negative().values();
+
+        assertThat(array).isNotNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Negative - case 2")
+    void negative_case2() {
+        double[] values = {5, 3, 1, 4, 2, 1};
+        double[] expected = {-5, -3, -1, -4, -2, -1};
+
+        Array array = new Array(values);
+        double[] actual = array.negative().values();
+
+        assertThat(array).isNotNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
     private double[] fillExpected(int size, double value) {
         double[] expected = new double[size];
         for (int i = 0; i < size; ++i) {
